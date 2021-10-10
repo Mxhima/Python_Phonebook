c9d02c153fe555d5b3a3df96eced1da88f19704e
Duvidu Nishad Karunasingha <71138026+Duvidunishad@users.noreply.github.com>
2021-10-10 21:07:48 +0530

    update the  code

--- python_Phonebook/main.py
+++ python_Phonebook/main.py
@@ -45,7 +45,24 @@ def show_main_menu():
 ''' Search contacts'''
 def search_contact_record():
     ''' This function is used to searches a specific contact record '''
+search_name = input("Enter First name for Searching contact record: ")
+rem_name = search_name[1:]
+first_char = search_name[0]
+search_name = first_char.upper() + rem_name
+file1 = open(file_name, "r+")
+file_contents = file1.readlines()
 
+
+
+found = False
+for line in file_contents:
+if search_name in line:
+print("Your Required Contact Record is:", end=" ")
+print(line)
+found = True
+break
+if found == False:
+print("There's no contact Record in Phone Book with name = " + search_name)
 
 '''first name input'''
 def input_fname():
